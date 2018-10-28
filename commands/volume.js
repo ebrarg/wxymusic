38c6faeac19e4527c799153a69d2d392e4a34948
ebrarg <42240618+ebrarg@users.noreply.github.com>
2018-10-28 20:59:40 +0300

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,7 +4,7 @@ const config = require('../settiings/config.json');
 module.exports.run = async (bot, message, args) => {
 
     let queue = bot.queue.get(message.guild.id);
-    if (!queue) return [message.delete(), utils.timed_msg('⚠ Muzik calmiyor.', 5000)];
+    if (!queue) return [message.delete(), utils.timed_msg('Muzik calmiyor.', 5000)];
     
     if (!args[0]) return [message.delete(), message.channel.send(`suanki ses: **${queue.volume}/100**`)];
     if (isNaN(args[0])) return [message.delete(), utils.timed_msg(utils.cmd_fail(`${message.author}, lutfen 0 ile 100 arasi ses gir!`, `${config.prefix}volume <volume>`), 5000)];
