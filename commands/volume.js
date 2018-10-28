fef71cc25fef7c79ecc44bad2803b4753e499d8c
ebrarg <42240618+ebrarg@users.noreply.github.com>
2018-10-28 20:59:27 +0300

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -6,14 +6,14 @@ module.exports.run = async (bot, message, args) => {
     let queue = bot.queue.get(message.guild.id);
     if (!queue) return [message.delete(), utils.timed_msg('⚠ Muzik calmiyor.', 5000)];
     
-    if (!args[0]) return [message.delete(), message.channel.send(`🎵 suanki ses: **${queue.volume}/100**`)];
+    if (!args[0]) return [message.delete(), message.channel.send(`suanki ses: **${queue.volume}/100**`)];
     if (isNaN(args[0])) return [message.delete(), utils.timed_msg(utils.cmd_fail(`${message.author}, lutfen 0 ile 100 arasi ses gir!`, `${config.prefix}volume <volume>`), 5000)];
     if (args[0] < 0 || args[0] > 100) return [message.delete(), utils.timed_msg(utils.cmd_fail(`${message.author}, lutfen 0 ile 100 arasi ses gir!`, `${config.prefix}volume <volume>`), 5000)];
 
     queue.volume = args[0];
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
 
-    return message.channel.send(`🎵 Ses suan icin **${queue.volume}/100**`);
+    return message.channel.send(`Ses suan icin **${queue.volume}/100**`);
 };
 
 module.exports.help = {
